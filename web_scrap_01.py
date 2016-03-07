e307a1736fc1248fb030455a354c711cace1b978
tkabelan@hotmail.com <tkabelan@hotmail.com>
2016-03-07 15:24:48 +0100

    changes for webscrap 1

--- web_scrap_01.py
+++ web_scrap_01.py
@@ -8,8 +8,8 @@ import socket
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysocket.connect(('www.py4inf.com', 80))
 
-#mysocket.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
-mysocket.send('GET http://www.publications.parliament.uk/pa/bills/lbill/2015-2016/0056/en/16056en01.htm HTTP/1.0\n\n')
+mysocket.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
+
 
 while True:
     data = mysocket.recv(650)
